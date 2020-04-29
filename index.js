6b3ff74734a8de6eef407656489994da190f37a8
Numan Mir <nmir@uoguelph.ca>
2020-04-28 20:58:24 -0400

    adding tweets, changing interval to 2 hours

--- index.js
+++ index.js
@@ -13,12 +13,22 @@ let T = new Twit({
 let quotes1 = [];
 quotes1[0] = "Are you at ease, at this moment?";
 quotes1[1] = "Ask yourself: what's going on inside me, right now?";
-quotes1[2] = "Pause for a few moments; take 5 deep breaths, be with each breath.";
+quotes1[2] = "Pause for a few moments; take 5 deep breaths, be with each one.";
 quotes1[3] = "How do you feel right now? Observe your emotions without judging them.";
+quotes1[4] = '"Almost everything will work again if you unplug if you unplug it for a few minutes, including you." - Anne Lamott';
+quotes1[5] = "As within, so without";
+quotes1[6] = "The clear sky is always there if you seek it.";
+quotes1[7] = "Never underestimate the clarity that comes with a calm mind.";
+quotes1[8] = "See if you can catch yourself complaining. Let go of resistance, then act once you have accepted the situation.";
+quotes1[9] = "Become aware of how fear and worry tend to distort reality and make things worse than they seem.";
+quotes1[10] = "Whatever you plan to do in the next five minutes, do it fully. Put away all distractions, and live every moment of it with all of your presence.";
+
+
+
 
 
 function post_tweet() { 
-    let r = Math.floor(Math.random()*3);
+    let r = Math.floor(Math.random()*10);
 
     let tweet_to_post = {
         status: quotes1[r]
@@ -36,7 +46,8 @@ function post_tweet() {
     T.post('statuses/update', tweet_to_post, check_post);
 }
 
-setInterval(post_tweet, 1000*30); //1000*60*120 every two hours
+post_tweet();
+setInterval(post_tweet, 1000*60*120); //1000*60*120 every two hours
 
 
 
